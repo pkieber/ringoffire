ecc38219578216c39436e95ca02758e92b616cbd
pkieber <113725311+pkieber@users.noreply.github.com>
2023-04-30 23:21:30 +0200

    install firebase and inject firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  title = 'ringoffire';
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-aedc9',
+    appId: '1:174352089609:web:a31438c4185ff855b2aed8',
+    storageBucket: 'ring-of-fire-aedc9.appspot.com',
+    apiKey: 'AIzaSyA9tQ74navj276_ngeGhvHEFAQrLSXU5as',
+    authDomain: 'ring-of-fire-aedc9.firebaseapp.com',
+    messagingSenderId: '174352089609',
+  },};
