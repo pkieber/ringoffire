2b2007b114b843d9e48e23f80f69b472e42fc6e2
Pat Kieber <113725311+pkieber@users.noreply.github.com>
2023-08-11 15:06:04 +0200

    Delete src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  firebase: {
-    projectId: '...',
-    appId: '...',
-    storageBucket: '...',
-    apiKey: '...',
-    authDomain: '...',
-    messagingSenderId: '...',
-  },};
